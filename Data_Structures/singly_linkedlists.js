74658d67828cf2862639f1c3376e5ba5559e3f06
osgood gunawan <ogunawan@ucsd.edu>
2020-07-29 18:36:25 -0400

    unshift

--- Data_Structures/singly_linkedlists.js
+++ Data_Structures/singly_linkedlists.js
@@ -12,7 +12,7 @@ class SinglyLinkedList{
         this.length=0
     }
     
-    // Implementing push in singly linkedlist
+    // Implementing push in singly linkedlist (add value to the next tail )
     push(val){
         const newNode= new Node(val)
         if(!this.head){
@@ -27,7 +27,7 @@ class SinglyLinkedList{
         return this
     }
 
-    // Implementing pop 
+    // Implementing pop (remove value from the the tail  )
     pop(){
         if(!this.head) return undefined;
         let current= this.head;
@@ -48,6 +48,36 @@ class SinglyLinkedList{
 
     }
 
+    shift(){
+        if (!this.head)return undefined
+        let currentHead=this.head;
+        this.head= currentHead.next;
+        this.length--;
+        if(this.length===0){
+            this.tail=null
+        }
+        return currentHead
+
+    }
+
+    unshift(val){
+       
+        const newNode= new Node(val)
+        if(!this.head){
+            this.head=newNode
+            this.tail=this.head ;
+        }
+        else{
+            newNode.next=this.head ;
+            this.head= newNode ;
+
+        }
+        this.length ++;
+        return this ;
+        
+
+    }
+
 
 
 }
@@ -57,12 +87,17 @@ let list = new SinglyLinkedList()
 list.push("Hello")
 list.push("World")
 list.push("MR.OG")
-list.pop()
-list.pop()
-list.pop()
+// list.pop()
+// list.pop()
+// list.pop()
 // list.pop()
 // list.push("WHADUP")
-console.log(list.pop())
+list.shift()
+list.shift()
+// list.shift()
+// list.shift()
+list.unshift("welcome!!")
+console.log(list)
 
 // console.log(list.head)
 // console.log(list.head.next)
