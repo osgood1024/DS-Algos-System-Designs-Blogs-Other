aede2771ad99f99833947ba58c83ec1a79c68599
osgood gunawan <ogunawan@ucsd.edu>
2020-07-29 17:51:16 -0400

    pop implementation

--- Data_Structures/singly_linkedlists.js
+++ Data_Structures/singly_linkedlists.js
@@ -12,6 +12,7 @@ class SinglyLinkedList{
         this.length=0
     }
     
+    // Implementing push in singly linkedlist
     push(val){
         const newNode= new Node(val)
         if(!this.head){
@@ -24,8 +25,31 @@ class SinglyLinkedList{
         }
         this.length++ ;
         return this
+    }
+
+    // Implementing pop 
+    pop(){
+        if(!this.head) return undefined;
+        let current= this.head;
+        let newTail= current;
+        while(current.next){
+            newTail=current;
+            current=current.next;
+        }
+        this.tail=newTail;
+        this.tail.next=null
+        this.length--;
+        //Here you want to make sure that head and tail are null if the length is zero or not there are always head and tail 
+        if(this.length===0){
+            this.head= null;
+            this.tail= null
+        }
+        return current
 
     }
+
+
+
 }
 
 
@@ -33,9 +57,16 @@ let list = new SinglyLinkedList()
 list.push("Hello")
 list.push("World")
 list.push("MR.OG")
-console.log(list.head)
-console.log(list.head.next)
-console.log(list.head.next.next)
+list.pop()
+list.pop()
+list.pop()
+// list.pop()
+// list.push("WHADUP")
+console.log(list.pop())
+
+// console.log(list.head)
+// console.log(list.head.next)
+// console.log(list.head.next.next)
 
 
 // let first = new Node("Hi")
