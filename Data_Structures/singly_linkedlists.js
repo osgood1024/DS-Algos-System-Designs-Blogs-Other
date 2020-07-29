24a5f375b06b14970ecb328182214ef7b6200c71
osgood gunawan <ogunawan@ucsd.edu>
2020-07-29 16:50:23 -0400

    push implementation

--- Data_Structures/singly_linkedlists.js
+++ Data_Structures/singly_linkedlists.js
@@ -1,1 +1,44 @@
-console.log("test")
+class Node{
+    constructor(val){
+        this.val=val;
+        this.next=null;
+    }
+}
+
+class SinglyLinkedList{
+    constructor(){
+        this.head=null;
+        this.tail=null;
+        this.length=0
+    }
+    
+    push(val){
+        const newNode= new Node(val)
+        if(!this.head){
+            this.head=newNode
+            this.tail=this.head
+        
+        }else{
+            this.tail.next=newNode;
+            this.tail=newNode;
+        }
+        this.length++ ;
+        return this
+
+    }
+}
+
+
+let list = new SinglyLinkedList()
+list.push("Hello")
+list.push("World")
+list.push("MR.OG")
+console.log(list.head)
+console.log(list.head.next)
+console.log(list.head.next.next)
+
+
+// let first = new Node("Hi")
+// first.next= new Node("there")
+// first.next.next=new Node("how")
+// console.log(first)
