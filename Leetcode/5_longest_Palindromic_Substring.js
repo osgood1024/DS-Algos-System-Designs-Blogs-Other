71d784484fa1acca0c8f7a5741901cb83ee3b3d3
osgood gunawan <ogunawan@ucsd.edu>
2020-08-06 17:34:54 -0400

    Leetcode new file up

---
+++ Leetcode/5_longest_Palindromic_Substring.js
@@ -0,0 +1,27 @@
+
+// Leetcode #5 Longest Palindromic Substring
+
+let longestPalindrome = function(s) {
+    // console.log(s[-1])
+      let res = "";
+      let cur = "";
+      for(let i = 0; i < s.length; i ++){
+          for(let j = i; j < i + 2; j ++){
+              let left = i;
+              let right = j;
+              console.log(left,right,i,j)
+              while(s[left] && s[left] === s[right]){
+                  cur = s.substring(left, right + 1);
+                  if(cur.length > res.length) res = cur;
+                  left --;
+                  right ++;
+                // console.log(cur,res,left,right)
+              }
+              // console.log(i,j)
+          }
+      }
+      return res;
+  };
+  
+  
+  longestPalindrome("babad")
