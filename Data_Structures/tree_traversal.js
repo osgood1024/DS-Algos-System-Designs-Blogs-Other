2a51ddf1a3f3e138db2788aa33d6048114cfc86e
osgood gunawan <ogunawan@ucsd.edu>
2020-10-10 21:39:53 -0400

    BFS&&DFS

---
+++ Data_Structures/tree_traversal.js
@@ -0,0 +1,86 @@
+class Node{
+    constructor(val){
+        this.val=val;
+        this.left=null;
+        this.right=null;
+    }
+}
+
+class BinarySearchTree{
+    constructor(){
+        this.root=null;
+    }
+    insert(val){
+        let newNode=new Node(val); 
+        if(this.root===null){
+            this.root=newNode; 
+            return this;
+        }
+
+        let current =this.root;
+        while(true){
+            if(val===current.val) return undefined;
+            if(val < current.val){
+               if(current.left===null){
+                   current.left=newNode;
+                   return this
+               }
+               current=current.left;
+            }else{
+                if(current.right===null){
+                    current.right= newNode;
+                    return this;
+                }
+
+                current =current.right;
+            }
+        }
+
+    }
+    BFS(){
+        let data=[], 
+            queue=[], 
+            node=this.root //node can be undefined intially 
+        queue.push(node);
+
+        while(queue.length){
+            node=queue.shift()
+            data.push(node.val);
+            if(node.left)queue.push(node.left);
+            if (node.right)queue.push(node.right);
+        }
+        return data
+    }
+
+    //recursively => traverse on the left first and the right 
+    DFSPreorder(){
+        let data=[];
+        
+        function traverse(node){
+            data.push(node.val)
+            if(node.left)traverse(node.left);
+            if(node.right)traverse(node.right);
+        }
+        traverse(this.root);
+        return data 
+
+
+    }
+
+}
+
+
+const tree= new BinarySearchTree()
+
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3)
+tree.insert(8)
+tree.insert(20)
+
+//tree.BFS()
+
+console.log(tree.DFSPreorder())
+
+
