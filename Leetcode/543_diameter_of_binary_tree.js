bf41f9198261cc246c9d73a9455ae40e0ac7ae00
osgood gunawan <ogunawan@ucsd.edu>
2020-10-18 02:01:21 -0400

    leetcode #543

---
+++ Leetcode/543_diameter_of_binary_tree.js
@@ -0,0 +1,38 @@
+
+// Leetcode #543 Diameter Of Binary Tree
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree = function(root) {
+    
+    let ans = 0;
+   if (!root){
+        return 0
+    }
+
+    
+    const helper=function (root){
+        
+        if (!root){
+            return 0
+        }
+        let l = helper(root.left)
+        let r = helper(root.right)
+        ans = Math.max(ans, l + r)
+        return 1 + Math.max(l,r)
+        
+    }
+    
+    helper(root);
+    return ans;
+    
+};
