2a26f04049af3cc388260ee50a4cb5012b98eb40
osgood gunawan <ogunawan@ucsd.edu>
2020-10-24 18:38:54 -0400

    update on leetcode 11

--- Leetcode/11_container_with_most_water.js
+++ Leetcode/11_container_with_most_water.js
@@ -1,5 +1,9 @@
 // Leetcode #11  Container with Most Water
 
+/**
+ * O(n) time complexity space complexity O(1)
+ * Two pointers approach 
+*/
 const maxArea=function(height){
     let maxArea=0
     let left=0
@@ -12,6 +16,29 @@ const maxArea=function(height){
 
     return maxArea
 }
+
+
+/**
+ * Time Complexity O(n^2)
+ * Space Complexity O(1)
+
+
+var maxArea = function(height) {
+    
+    let max=Number.MIN_SAFE_INTEGER
+    
+    for(let i =height.length-1;i>0;i--){
+        for(let j=0;j<i;j++){
+            let length= height[i] > height[j] ? height[j] : height[i]
+            let area=(i-j)*length
+            max=Math.max(max,area)
+        }
+    }
+    
+    return max
+    
+};
+*/
 
 let height=[1,8,6,2,5,4,8,3,7]
 console.log(maxArea(height))
