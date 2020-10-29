d3549732e9023831cf6bd4d5b4b73ab7392f44d4
osgood gunawan <ogunawan@ucsd.edu>
2020-10-29 18:43:47 -0400

    Leetcode #234

---
+++ Leetcode/234_palindrome_linked_list.js
@@ -0,0 +1,35 @@
+/**
+ * Leetcode #234 Palindrom Linked List 
+ */
+
+ /**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+
+ //O(N) Time complexity however not sure about O(1) Space Complexity 
+var isPalindrome = function(head) {
+    let arr = [];
+    
+    while(head !== null){
+        arr.push(head.val);
+        head = head.next;
+    }
+    
+    const reversed = [...arr].reverse();
+    
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] !== reversed[i]){
+            return false;
+        }
+    }
+    
+    return true;
+};
