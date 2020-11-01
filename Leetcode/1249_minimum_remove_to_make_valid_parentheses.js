f79694965a3b7cbc879c8ae0639806adaac098a8
osgood gunawan <ogunawan@ucsd.edu>
2020-10-31 23:55:01 -0400

    leetcode #1249

---
+++ Leetcode/1249_minimum_remove_to_make_valid_parentheses.js
@@ -0,0 +1,38 @@
+/**
+ * Leetcod #1249 Minimum Remove To Make Valid Parentheses
+ * 
+ * 
+ */
+
+
+ const minRomoveToMakeValid = function(s){
+     let str=s.split('')
+     let stack=[]
+
+     for(let i =0;i<str.length;i++){
+         if(str[i]==="("){
+             stack.push(i)
+         }
+         else if(str[i]===")"){
+            if(stack.length==0){
+                str[i]=''
+            }
+            else{
+                stack.pop()
+            }
+         }
+     }
+     while(stack.length!==0){
+         str[stack.pop()]=''
+     }
+
+     return str.join('')
+ }
+// let s="a)b(c)d"
+// let s="lee(t(c)o)de)"
+// let s="(a(b(c)d)"
+
+let s="))(("
+
+
+ console.log(minRomoveToMakeValid(s))
