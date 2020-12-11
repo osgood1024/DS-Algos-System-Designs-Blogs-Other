39cc44b63691797073d6071da64436ea7b4bd545
osgood gunawan <ogunawan@ucsd.edu>
2020-12-11 16:08:20 -0500

    extracMax

--- Data_Structures/binary_heap.js
+++ Data_Structures/binary_heap.js
@@ -1,6 +1,6 @@
 class  MaxBinaryHeap{
     constructor(){
-        this.values=[41,39,33,18,27,12]
+        this.values=[41,39,33]
     }
 
     insert(element){
@@ -18,9 +18,51 @@ class  MaxBinaryHeap{
             this.values[parentIdx]=element;
             this.values[idx]=parent
             idx= parentIdx
-            
         }
     }
+
+    extractMax(){
+        const max=this.values[0]
+        const end=this.values.pop();
+        if(this.values.length>0){
+            this.values[0]=end;
+            this.sinkDown()
+        }
+        return max
+    }
+    sinkDown(){
+        let idx=0
+        const length=this.values.length;
+        const element=this.values[0]
+        while(true){
+            let leftChildIdx=2*idx+1;
+            let rightChildIdx=2*idx+2;
+            let leftChild,rightChild
+            let swap=null
+
+            if(leftChildIdx<length){
+                leftChild=this.values[leftChildIdx]
+                if(leftChild>element){
+                    swap=leftChildIdx;
+                }
+            }
+
+            if(rightChildIdx<length){
+                rightChild=this.values[rightChildIdx]
+                if( (swap === null && rightChild> element) || (swap!==null && rightChild>leftChild) ){
+                    swap=rightChildIdx
+                }
+            }
+
+
+            if(swap===null) break;
+            this.values[idx]=this.values[swap]
+            this.values[swap]=element;
+            idx=swap
+        }
+
+    }
+
 }
 
 
@@ -28,7 +70,17 @@ let heap= new MaxBinaryHeap()
 heap.insert(55);
 heap.insert(1);
 heap.insert(45);
-heap.insert(100)
+heap.insert(100);
+heap.insert(69)
+heap.extractMax();
+heap.extractMax();
+heap.extractMax();
+
+// heap.extractMax();
+// heap.extractMax();
+// heap.extractMax();
+// heap.extractMax();
+
 
 console.log(heap)
 
