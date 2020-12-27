95ab2a52d17f80ec5d7fb10f7e0aa546f2326c74
osgood gunawan <ogunawan@ucsd.edu>
2020-12-26 23:59:07 -0500

    leetcode#735

---
+++ Leetcode/735_Asteroid_Collision.js
@@ -0,0 +1,35 @@
+
+//Leetcode #735 Asteroid Collision 
+
+// This question is similar with the 'space battle' in bsio
+
+
+
+var asteroidCollision = function(asteroids) {
+    
+    let stack=[]
+    
+    for(let i =0;i<asteroids.length;i++){
+        let curr=asteroids[i]
+        if((stack.length===0 || stack[stack.length-1]<0)&& curr<0){
+           stack.push(curr)
+         }
+        else if(curr>0){
+            stack.push(curr)
+        }else{
+            let pop=stack.pop()
+            
+            if(Math.abs(pop)<Math.abs(curr)){
+                i--
+            }else if (Math.abs(pop)>Math.abs(curr)){
+                stack.push(pop)
+            }else{
+                continue ;
+            }
+        }
+    }
+    
+    return stack
+    
+    
+};
