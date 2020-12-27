433a2894c5cb2623dc967b167d8c4f7572b79afd
osgood gunawan <ogunawan@ucsd.edu>
2020-12-27 03:07:42 -0500

    leetcode_724

---
+++ Leetcode/724_Find_Pivot_Index.js
@@ -0,0 +1,20 @@
+
+
+// Leetcode #724 Find Pivot Index 
+
+// Prefix Sum 
+
+var pivotIndex = function(nums) {
+    
+    let left_sum=0
+    
+    let sum=nums.reduce((sum,acc)=>sum+acc,0)
+    for(let i =0;i<nums.length;i++){
+        let right_sum=sum-left_sum
+        left_sum+=nums[i]
+        if(right_sum===left_sum)return i
+    }
+    
+    return -1
+
+};
