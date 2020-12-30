87029de348c96bb1f191ac3b2adca7f4b49dc28f
osgood gunawan <ogunawan@ucsd.edu>
2020-12-30 16:07:06 -0500

    leetcode#14

---
+++ Leetcode/14_Longest_Common_Prefix.js
@@ -0,0 +1,20 @@
+// 14 Longest Common Prefix
+// There are different ways of solving this problem
+
+// Vertical Scanning
+const longestCommonPrefix=function(strs){
+
+    if(strs==null ||strs.length==0)return ""
+
+    for(let i =0;i<strs[0].length;i++){
+        let char=strs[0].charAt(i)
+        for(let j=1;j<strs.length;j++){
+            if(strs[j].charAt(i)!==char){
+                // strs[0].slice(0,i) => this is also works 
+                return strs[0].substring(0,i)
+            }
+        }
+    }
+
+    return strs[0]
+}
