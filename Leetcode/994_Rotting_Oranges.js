2ff488afb0034d5337a0afec024829402d1c70ab
osgood gunawan <ogunawan@ucsd.edu>
2021-01-18 13:05:33 -0500

    update_#994

--- Leetcode/994_Rotting_Oranges.js
+++ Leetcode/994_Rotting_Oranges.js
@@ -7,7 +7,7 @@ const orangesRotting=function(grid){
     let minute=0
     let rottenOranges=[]
 
-    // Here we are trying to find all the total fresh Oranges and all the rotten Oranges coordinate (x,y) in 2d 
+    // Here we are trying to find all the total fresh oranges and all the rotten oranges coordinate (x,y) in 2d 
     for(let i =0;i<grid.length;i++){
         for(let j=0;j<grid[i].length;j++){
             if(grid[i][j]===1)totalfreshOranges++
@@ -28,6 +28,7 @@ const orangesRotting=function(grid){
     */
     while(totalfreshOranges && rottenOranges.length){
         // we want a new array (is like a temp) so we can finish traversing the rotten Oranges surrounding 
+        // Fresh oranges that is adjacent to the rotten oranges will be pushed into this and will become rotten oranges
         let rottingOranges=[]
 
         while(rottenOranges.length){
@@ -44,7 +45,7 @@ const orangesRotting=function(grid){
                 if(grid[x2]&& grid[x2][y2]===1 ){
                     totalfreshOranges--
                     grid[x2][y2]=2
-                    //Here is acting like queues,
+                    //Here is acting like stack,
                     //Everytime we find fresh oranges near by rotten orange 
                     //we want to get the coordinates of the fresh oranges since the fresh Oranges is becoming rotten. 
                      rottingOranges.push([x2,y2])
@@ -114,4 +115,71 @@ var orangesRotting = function(grid) {
 
 
 
+/**
+ * 
+ * 
+ * Blog version explaining
+ * 
+ * 
+ * 
+ * 
+var orangesRotting = function(grid) {
 
+    let totalfreshOranges = 0
+    let minute = 0
+    let rottenOranges = []
+
+    // Here we are trying to calculate how many fresh oranges we have 
+    // and find all the rotten oranges coordinate (where they locate (x,y) in 2d)
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            if (grid[i][j] === 1) totalfreshOranges++
+            if (grid[i][j] === 2) rottenOranges.push([i, j])
+        }
+    }
+
+    // If there is no fresh oranges at all in the grid then just return 0 
+    if (totalfreshOranges == 0) return 0
+
+    //check 4 directionally adjacent of rotten oranges (West, East, North, South)
+    const coordinate = [[0, -1],[0, 1],[-1, 0],[1, 0]]
+
+    //  Traverse the 2d matrix, while there are still fresh oranges left and rotten oranges left to be spread 
+    while (totalfreshOranges && rottenOranges.length) {
+        // fresh oranges that is adjacent to the rotten oranges will be store into this array.
+        let rottingOranges = []
+        while (rottenOranges.length) {
+
+            let [x, y] = rottenOranges.shift()
+            for (let i = 0; i < 4; i++) {
+                let [x2, y2] = [x + coordinate[i][0], y + coordinate[i][1]]
+
+                // If the adjacency of rotten orange is fresh orange, make it rotten, 
+                // decrease the total fresh oranges, and push the coordination of that fresh orange to rotten orrange 
+                // into the rottingOranges array (queue data structure incoming) 
+                if (grid[x2] && grid[x2][y2] === 1) {
+                    totalfreshOranges--
+                    grid[x2][y2] = 2
+                    rottingOranges.push([x2, y2])
+                }
+            }
+        }
+        // The rotting oranges, which are the one that just got rotten now become rotten oranges 
+        // In that way, we can increment the minute(the timestamp of each minute should be decide by 
+        // each of rotting oranges that get push into rotten oranges array => rottenOranges.length)
+        // It will keep spreading until there is nothing left 
+        rottenOranges = rottingOranges
+        minute++
+    }
+
+    // If there is any fresh oranges left, we want to return -1 else return the minute 
+    return totalfreshOranges ? -1 : minute
+
+};
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
