595166ec0d784025be9e19794c7813521ec1b087
osgood gunawan <ogunawan@ucsd.edu>
2021-01-25 11:56:35 -0500

    update_leetcode_#253

--- Leetcode/253_meeting_rooms_II.js
+++ Leetcode/253_meeting_rooms_II.js
@@ -1,23 +1,38 @@
 // #253 Meeting rooms II
 
+// Time complexity -O(NlogN)
+// Space complexity-O(N)
+
 var minMeetingRooms = function(intervals) {
     
-    
-    let starts=intervals.sort((a,b)=>a[0]-b[0])
-    
-    let ends=[...intervals].sort((a,b)=>a[1]-b[1])
-    let rooms=0
-    let j=0
-    
-    for(let i =0;i<intervals.length;i++){
-        if(starts[i][0]<ends[j][1]){
-            rooms++
-        }else{
-            j++
+   
+        // Sort the intervals by the start time 
+        let start = intervals.sort((a, b) => a[0] - b[0])
+
+        // Create a copy array and sort the intervals by the end time
+        let end = [...intervals].sort((a, b) => a[1] - b[1])
+
+        // Keep track on how many room(s) is/are needed
+        let rooms = 0
+
+        // Two pointers incoming 
+        let j = 0
+
+        for (let i = 0; i < intervals.length; i++){
+            //whenever there is a start meeting, we need to add one room. 
+            // But before adding rooms, we check to see if any previous meeting ends,
+            //  which is why we check start with the first end. 
+            if (start[i][0] < end[j][1]) {
+                rooms++
+            } 
+            // When the start is bigger than end, it means at this time one of the previous meeting ends, and it can take and reuse that room.
+            //  Then the next meeting need to compare with the second end because the first end's room is already taken. 
+            else {
+                j++
+            }
         }
 
-    }
-    
-    return rooms
+        return rooms
+
     
 };
