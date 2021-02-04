2a8fa10223ffd879dd37869991930aaed60c2cd7
osgood gunawan <ogunawan@ucsd.edu>
2021-02-03 20:52:18 -0500

    graph.js

---
+++ Data_Structures/graph.js
@@ -0,0 +1,11 @@
+class Graph{
+    constructor(){
+        this.adjacencyList={}
+    }
+
+    addVertex(vertex){
+        if(!this.adjacencyList[vertex])
+        this.adjacencyList[vertex]=[] ;
+
+    }
+}
